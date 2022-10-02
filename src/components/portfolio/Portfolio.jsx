6620f3f0112d2d6eac7a27d711f3bf6091f97bd4
SUDHANSHU KESARWANI <79747228+sudhanshukes65@users.noreply.github.com>
2022-10-03 00:49:13 +0530

    changes in project section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
@@ -14,7 +14,7 @@ const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+      <h2>Projects</h2>
 
       <div className="container portfolio__container">
         <article className="portfolio__item">
@@ -23,10 +23,10 @@ const Portfolio = () => {
             <img src={IMG1 } alt="" />
 
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>This is a online food order website (only frontend)</h3>
             <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">Github</a>
-            <a href="https://github.com" className="btn btn-primary"  target="_blank">Live Demo</a>
+            <a href="https://github.com/sudhanshukes65/Food_Order_Website_Project-main" className="btn" target="_blank">Github</a>
+            <a href="https://foodorderbysk.netlify.app/" className="btn btn-primary"  target="_blank">Live Demo</a>
 
             </div>
         </article>
@@ -34,13 +34,13 @@ const Portfolio = () => {
         <article className="portfolio__item">
 
           <div className="portfolio__item-image">
-            <img src={IMG1 } alt="" />
+            <img src={IMG2 } alt="" />
 
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>This is a Drum kit webiste (With different drum sounds)</h3>
             <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">Github</a>
-            <a href="https://github.com" className="btn btn-primary"  target="_blank">Live Demo</a>
+            <a href="https://github.com/sudhanshukes65/drum-kit" className="btn" target="_blank">Github</a>
+            <a href="https://drum-kit-bysk.netlify.app/" className="btn btn-primary"  target="_blank">Live Demo</a>
 
             </div>
         </article>
